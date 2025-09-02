27968e9eda46f5d37e89f431b1076415c201be34
abdelkarim elhafiani <abdelkarmielhafiani@gmail.com>
2025-09-02 22:45:10 +0200

    feat modifying user class

--- domaine/Entities/User.cs
+++ domaine/Entities/User.cs
@@ -3,5 +3,6 @@ public class User{
     public string FirstName{get;set;}=null!;
     public string LastName{get;set;}=null!;
     public string Email{get;set;}=null!;
-    public string Password{get;set;}=null!;
+    //this is not a good code
+    public string Password { get; set; } = null!;
 }
