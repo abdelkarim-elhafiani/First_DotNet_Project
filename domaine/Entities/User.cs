2ef0adbd383911e43122c40ea60308be698346f1
abdelkarim elhafiani <abdelkarmielhafiani@gmail.com>
2025-09-02 23:13:14 +0200

    adding a comment to user class

--- domaine/Entities/User.cs
+++ domaine/Entities/User.cs
@@ -1,7 +1,8 @@
 public class User{
     public Guid Id =Guid.NewGuid();
     public string FirstName{get;set;}=null!;
-    public string LastName{get;set;}=null!;
+    //this is a great code
+    public string LastName { get; set; } = null!;
     public string Email{get;set;}=null!;
     //this is not a good code
     public string Password { get; set; } = null!;
